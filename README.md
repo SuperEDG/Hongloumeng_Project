f1424e87ddb02563d04fa1d9314cbbafe74042e9
GavinZhao19 <J.Zhao33@newcastle.ac.uk>
2023-07-02 12:51:52 +0800

    Update READMD.md

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# Hongloumeng_Project
+# Dream of the Red Chamber Chatbot Project
+
+## Abstract
+
+This project aims to enhance chatbot engagement by leveraging unique personalities and speech patterns extracted from character dialogues in literary works. We utilized 《红楼梦》(Dream of the Red Chamber) as a data source, developed an automated system for efficient extraction and mapping of character dialogues and personality traits. This approach results in chatbots more resembling human conversation, proven by evaluations. All datasets and models are available in this GitHub repository.
+
+## Repository Structure
+
+The repository consists of three main directories: `data`, `model`, and `process`.
+
+### Data
+
+- `resources`: Contains the original data.
+- `processed`: Contains the processed data.
+
+### Process
+
+- `chinese_traits_extractor.py`: Builds the Chinese traits word library.
+- `clean_and_store_reviews.py`: Cleans the book's reviews.
+- `extractDialogues.py`: Extracts dialogues from the book.
+
+### Model
+
+- `2-NER`: Contains the Named Entity Recognition model for extracting character names.
+- `3-LCPM`: Constructs the Literary Character Personality Map (LCPM) and selects suitable characters.
+- `4-DialogueAgent`: Builds the final chatbot model using the personality maps and dialogues.
+
+## Replicating the Project
+
+To use this system with a new book:
+
+1. **Book Addition**: Add the new book to `data/resources/books`.
+
+2. **Dialogue Extraction**: Run `extractDialogues.py` in the `process` directory. Extracted dialogues will be stored in `data/processed/books`.
+
+3. **Dialogue and Character Extraction**: Run `extract_dialogues.py` in `model/2-NER`. This will extract dialogues and character names.
+
+4. **Character Selection**: 
+   - First, search for corresponding character reviews and store them in `data/resources/Reviews`.
+   - Then run `clean_and_store_reviews.py` in the `process` directory. The cleaned reviews will be stored in `data/processed/LCPM`.
+   - Run `generateLCPM.py` in `model/3-LCPM` to create the character personality map.
+   - Run `tsne_characters.py` for visualizing character personalities, and `character_selection.py` to select suitable characters.
+
+5. **Chatbot Model Construction**: Use the constructed dataset to build a generative model in `model/4-DialogueAgent`.
+
+## License
+
+This project is under the MIT license. Refer to the LICENSE file for more details.
--- model/3-LCPM/tsne_characters.py
+++ model/3-LCPM/tsne_characters.py
@@ -1,84 +1,90 @@
 from itertools import chain
-
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 from sklearn.manifold import TSNE
 
+# Define groups of characters
 groups = {
-    'g1': ['Xiren', 'Qinkeqin'],
-    'g2': ['Jiabaoyu', 'Liwan', 'Jiatanchun'],
-    'g3': ['Lindaiyu', 'Qingwen', 'Xingfuren'],
-    'g4': ['Miaoyu', 'Jiaxichun', 'Xuebaochai'],
-    'g5': ['Youerjie', 'Shixiangyun', 'Jiayinchun'],
+    'g1': ['Jia Baoyu', 'Jia Yingchun', 'Jia Yuanchun', 'Jia Xichun', 'Xi Ren', 'Qin Keqing'],
+    'g2': ['Lin Daiyu', 'Qing Wen', 'Miao Yu'],
+    'g3': ['Xue Baochai', 'Li Wan', 'Jia She', 'Wang Furen', 'Xing Furen'],
+    'g4': ['Shi Xiangyun', 'Xiang Ling', 'You Erjie', 'You Sanjie', 'Jia Tanchun', 'Jia Qiaojie'],
+    'g5': ['Wang Xifeng', 'Jia Mu', 'Jia Zheng', 'Jia Zhen']
 }
 
-
-def plot_tsne_series(X, y, perplexies, n_iter=5000, alpha=0.1, palette=None):
-
-    # we need to filter due to the t-SNE limitation.
+# Define function to plot t-SNE visualizations for different perplexity values
+def plot_tsne_series(X, y, perplexies, n_iter=1000, alpha=0.1, palette=None):
+    # Filter perplexities due to the t-SNE limitation.
     perplexies = list(filter(lambda item: item < len(X), perplexies))
 
+    # Generate t-SNE embeddings for each perplexity
     embs_X = []
     for p in perplexies:
         tsne = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=p, n_iter=n_iter, random_state=0)
         emb_X = tsne.fit_transform(X)
         embs_X.append(emb_X)
 
+    # Create a flat list of x and y coordinates and corresponding perplexity values
     c1 = list(chain(*[list(embs_X[i][:, 0]) for i in range(len(perplexies))]))
     c2 = list(chain(*[list(embs_X[i][:, 1]) for i in range(len(perplexies))]))
     arr = list(chain(*[[p] * len(embs_X[0]) for p in perplexies]))
 
+    # Prepare the DataFrame for plotting
     tsne_data = pd.DataFrame()
     tsne_data["comp-1"] = c1
     tsne_data["comp-2"] = c2
     tsne_data["perplexy"] = arr
     tsne_data["Groups"] = list(chain(*[y for p in perplexies]))
 
+    # Create facet grid of scatter plots
     g = sns.FacetGrid(tsne_data, col="perplexy", hue="Groups", palette=palette)
     g.map(sns.scatterplot, "comp-1", "comp-2", alpha=alpha)
 
+    # Move the legend to upper right
     plt.legend(loc='upper left')
 
-    #plt.show()
+    # Save the figure
     plt.gcf().set_size_inches(8, 6)
     plt.savefig("tsne.png", bbox_inches='tight', dpi=200)
 
-
+# Function to normalize an array to a specified range
 def normalize(arr, t_min, t_max):
     norm_arr = []
     diff = t_max - t_min
-    diff_arr = max(arr) - min(arr)   
+    diff_arr = max(arr) - min(arr)
     for i in arr:
         temp = (((i - min(arr))*diff)/diff_arr) + t_min
         norm_arr.append(temp)
     return norm_arr
 
-
+# Function to create a group name
 def __make_group_name(v):
     return v[0] + " et. al."
 
-
+# Function to determine which group a character belongs to
 def __find_group(name):
     assert(isinstance(name, str))
     for k, v in groups.items():
         if name in v:
-            # group name.
+            # Return group name
             return __make_group_name(v)
-    return "_Other"
+    return "_Other"  # Default group name if character doesn't belong to any specified group
 
+# Read in the data
+df = pd.read_csv(r"C:\Users\Admin\Desktop\Project\LCPM-finall3.csv")
 
-df = pd.read_csv("model/LCPM/LCPM.csv")
-
+# Prepare X and y for the t-SNE plot
 X = []
 y = []
 for i, r in df.iterrows():
     X.append(normalize(np.array(r[1:]), 0, 1))
     y.append(r[0])
 
+# Convert to numpy array and plot
 X = np.array(X)
-plot_tsne_series(X=X, y=[__find_group(i) for i in y], perplexies=[2], n_iter=500, alpha=0.7,
+plot_tsne_series(X=X, y=[__find_group(i) for i in y], perplexies=[2], n_iter=1000, alpha=0.7,
                  palette={
                     "_Other": "gray",
                     __make_group_name(groups["g1"]): "red",
